3737180bb712d12b13cf8bff48ecc55fce2e510a
XiAceLite <150305392+XiAce-Lite@users.noreply.github.com>
2026-05-09 10:04:42 +0900

    chore: update version to 1.0.8 in manifest, background, and popup files

--- background.js
+++ background.js
@@ -16,7 +16,7 @@ const DEFAULT_DURATION_MS = 240_000;
 
 /** MusicBrainz requires a descriptive User-Agent. */
 const MUSICBRAINZ_USER_AGENT =
-	'Chordwiki-AutoScroller/1.0.7 (https://github.com/)';
+	'Chordwiki-AutoScroller/1.0.9 (https://github.com/)';
 
 // -----------------------------------------------------------------------------
 // Options
